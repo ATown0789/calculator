4ed4a35617b2eed1b759cfa49cb1dd15eb121b77
Anthony Collini <collini.anthony@gmail.com>
2020-06-18 22:21:39 -0500

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,91 @@
 import React from 'react';
 import './App.css';
+import Buttons from './components/Buttons';
 
-function App() {
+let isNum = /[0-9]/;
+
+class App extends React.Component{
+	constructor(props){
+		super(props);
+		
+		this.state = {
+			previousVal: '0',
+			currentVal: '0',
+			allDisplay: '0'
+		}
+		
+		this.number = this.number.bind(this);
+		this.clear = this.clear.bind(this);
+		this.opperator = this.opperator.bind(this);
+		this.equals = this.equals.bind(this);
+	}
+	
+	equals(){
+		this.setState({
+			currentVal: this.state.currentVal[1]+this.state.currentVal[3]
+		})
+	}
+	
+	number(e){
+		let {currentVal: current, allDisplay: all} = this.state;
+		let val = e.target.value;
+		if(current.length === 1 && current === '0'){
+			if(val !== '0'){
+				this.setState({
+					currentVal: val,
+					allDisplay: all === '0' ? val : all + val
+				})
+			}	
+		}
+		else if(current.length === 1 && !isNum.test(current)){
+			this.setState({
+				currentVal: val,
+				allDisplay: val === '0' ? all : all + val
+			})
+		}
+		else{
+			this.setState({
+				currentVal: current + val,
+				allDisplay: all + val
+				
+			})
+		}
+		
+		
+	};
+	
+	opperator(e){
+		let opp = e.target.value
+		this.setState({
+			currentVal: opp,
+			allDisplay: this.state.allDisplay + opp
+		})
+	}	
+	
+	
+	clear(){
+		this.setState({
+			allDisplay: '0',
+			currentVal: '0',
+			previousVal: '0'
+		})
+		
+	}
+	
+	
+	render(){
   return (
     <div className="App">
       <h1>Hello World</h1>
+	  <Buttons 
+		number = {this.number}
+		clear = {this.clear}
+		opperator = {this.opperator}
+		equals = {this.equals}/>
+	  <h3 id = 'display'>currentDisplay: {this.state.currentVal}</h3>
+	  <h3>allDisplay: {this.state.allDisplay}</h3>
     </div>
-  );
+	)};
 }
 
 export default App;
---
+++ src/components/Buttons.js
@@ -0,0 +1,95 @@
+import React from 'react';
+
+class Buttons extends React.Component{
+	
+	
+	render(){
+		return(
+			<div>
+				<button 
+				id = 'equals'
+				onClick = {this.props.equals}>
+					=
+				</button>
+				<button 
+					id = 'zero' 
+					value = {0}
+					onClick = {this.props.number}>
+						0
+				</button>
+				<button 
+					id = 'one'
+					value = {1}
+					onClick = {this.props.number}>
+						1
+				</button>
+				<button 
+					id = 'two' 
+					value = {2}
+					onClick = {this.props.number}>
+						2
+				</button>
+				<button 
+					id = 'three' 
+					value = {3}
+					onClick = {this.props.number}>
+						3
+				</button>
+				<button 
+					id = 'four'
+					 value = {4}
+					 onClick = {this.props.number}>
+						4
+				</button>
+				<button 
+					id = 'five'
+					 value = {5}
+					 onClick = {this.props.number}>
+						5
+				</button>
+				<button 
+					id = 'six'
+					 value = {6}
+					 onClick = {this.props.number}>
+						6
+				</button>
+				<button 
+					id = 'seven'
+					 value = {7}
+					 onClick = {this.props.number}>
+						7
+				</button>
+				<button 
+					id = 'eight'
+					value = {8}
+					onClick = {this.props.number}>
+						8
+				</button>
+				<button 
+					id = 'nine'
+					 value = {9}
+					 onClick = {this.props.number}>
+						9
+				</button>
+				<button 
+					id = 'add'
+					onClick = {this.props.opperator}
+					value = '+'>
+						+
+				</button>
+				<button id = 'subtract'>-</button>
+				<button id = 'multiply'>x</button>
+				<button id = 'divide'>/</button>
+				<button id = 'decimal'>.</button>
+				<button 
+					id = 'clear'
+					onClick = {this.props.clear}>
+						clr
+				</button>
+			</div>
+		
+		)
+	}
+}
+
+export default Buttons;
